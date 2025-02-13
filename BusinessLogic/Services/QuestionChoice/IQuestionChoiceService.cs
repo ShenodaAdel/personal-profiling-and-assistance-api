2cb3ee4d9cdea7ac79e6af039565acf0e3171343
ShenodaAdel <shenodaadel110@gmail.com>
2025-02-13 15:32:16 +0200

    Finish CRUD operations in this QuestionChoice entity

---
+++ BusinessLogic/Services/QuestionChoice/IQuestionChoiceService.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLogic.DTOs;
+using BusinessLogic.Services.QuestionChoice.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Services.QuestionChoice
+{
+    public interface IQuestionChoiceService
+    {
+        Task<ResultDto> AddQuestionChoiceAsync(QuestionChoiceDto dto);
+        Task<ResultDto> UpdateQuestionChoiceAsync(int id, QuestionChoiceDto dto);
+        Task<ResultDto> DeleteQuestionChoiceAsync(int id);
+        Task<ResultDto> GetQuestionChoiceByIdAsync(int id);
+        Task<ResultDto> GetAllQuestionChoicesAsync();
+    }
+}
