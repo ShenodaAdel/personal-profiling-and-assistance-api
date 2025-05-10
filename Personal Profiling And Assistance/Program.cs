30037b160d5710c0388bf48d0b85ed7789299ea4
Shenoda <shenodaadel63@gmail.com>
2025-05-10 23:14:06 +0300

    ++

--- Personal Profiling And Assistance/Program.cs
+++ Personal Profiling And Assistance/Program.cs
@@ -90,7 +90,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseCors("AllowAngularApp"); // Apply the CORS policy
 app.UseAuthentication();
